bdb5a96ac8330107e107fe2b5a1de164605a4cb0
otavio b <otaviob22@gmail.com>
2026-03-16 22:31:19 +1100

    Initial Commit

---
+++ ios-combine-mvvm-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-combine-mvvm-swift
+//
+//  Created by otavio brito on 16/3/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios-combine-mvvm-swiftUITests/ios_combine_mvvm_swiftUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ios_combine_mvvm_swiftUITestsLaunchTests.swift
+//  ios-combine-mvvm-swiftUITests
+//
+//  Created by otavio brito on 16/3/2026.
+//
+
+import XCTest
+
+final class ios_combine_mvvm_swiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
