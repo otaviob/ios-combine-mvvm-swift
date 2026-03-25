36764d20e7be79f8e5fb3edf0ae472830e6ff977
otavio b <otaviob22@gmail.com>
2026-03-25 12:46:09 +1100

    Add ResultView details

--- ios-combine-mvvm-swift/View/ResultView.swift
+++ ios-combine-mvvm-swift/View/ResultView.swift
@@ -6,8 +6,63 @@
 //
 
 import UIKit
+import SnapKit
 
 class ResultView: UIView {
+    
+    /// propities
+    
+    private let headerLabel: UILabel = {
+        LabelFactory.build(text: "Total",
+                           font: ThemeFont.demiBold(offSize: 18))
+    }()
+    
+    private let amountPersonlabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        let text = NSMutableAttributedString(
+            string: "$0",
+            attributes: [
+                .font: ThemeFont.demiBold(offSize: 48)
+                ])
+        text.addAttributes([
+            .font: ThemeFont.bold(offSize: 24)
+        ], range: NSMakeRange(0, 1))
+        label.attributedText = text
+        return label
+    }()
+    
+    private let horizontalLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = ThemeColor.separator
+        return view
+    }()
+    
+    private lazy var vStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            headerLabel,
+            amountPersonlabel,
+            horizontalLineView,
+            builderSpacerView(height: 0),
+            hStackView
+        ])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    private lazy var hStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            AmountView(),
+            UIView(),
+            AmountView()
+        ])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    /// initializer
     
     init() {
         super.init(frame: .zero)
@@ -18,11 +73,47 @@ class ResultView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    /// layout
+    
     private func layout() {
-        backgroundColor = .gray
+        backgroundColor = .white
+        addSubview(vStackView)
+        vStackView.snp.makeConstraints { make in
+            make.top.equalTo(snp.top).offset(24)
+            make.leading.equalTo(snp.leading).offset(24)
+            make.trailing.equalTo(snp.trailing).offset(-24)
+            make.bottom.equalTo(snp.bottom).offset(-24)
+        }
+        horizontalLineView.snp.makeConstraints { make in
+            make.height.equalTo(2)
+        }
         
+        addShadow(offset: CGSize(width: 0, height: 3),
+                  color: .black,
+                  radius: 12.0,
+                  opacity: 0.1)
     }
     
+    private func builderSpacerView(height: CGFloat) -> UIView {
+        let view = UIView()
+        view.heightAnchor.constraint(equalToConstant: height).isActive = true
+        return view
+    }
     
 }
 
+class AmountView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .systemBlue
+    }
+}
---
+++ ios-combine-mvvm-swift/extension/UIView+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  UIView+Extension.swift
+//  ios-combine-mvvm-swift
+//
+//  Created by otavio brito on 25/3/2026.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addShadow(offset: CGSize, color: UIColor, radius: CGFloat, opacity: Float) {
+        layer.cornerRadius = radius
+        layer.masksToBounds = false
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+        layer.shadowRadius = radius
+        layer.shadowOpacity = opacity
+        let backgroundCGColor = backgroundColor?.cgColor
+        backgroundColor = nil
+        layer.backgroundColor = backgroundCGColor
+    }
+    
+}
+
